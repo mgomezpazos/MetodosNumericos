f9075c37753e67a9c5fd1d3ae30086405cb99ab3
Zoe <zvelazquezorzi@udesa.edu.ar>
2023-11-05 16:29:46 -0300

    lool el nano

--- Tp3/p2.py
+++ Tp3/p2.py
@@ -1,60 +1,3 @@
-# import numpy as np
-# import pandas as pd
-# from sklearn.decomposition import TruncatedSVD
-# from sklearn.metrics.pairwise import rbf_kernel
-# import matplotlib.pyplot as plt
-
-# # Carga el conjunto de datos
-# data = pd.read_csv('dataset02.csv')
-# X = data.values  # Convierte los datos a una matriz NumPy
-
-# # Valores de d a considerar
-# dims = [2, 4, 6, 20, X.shape[1]]  # Incluye la dimensionalidad original (p)
-
-# # Realiza la reducción de dimensionalidad para diferentes valores de d
-# for d in dims:
-#     # Aplica la descomposición SVD
-#     svd = TruncatedSVD(n_components=d)
-#     X_reduced = svd.fit_transform(X)
-    
-#     # Calcula las similitudes usando el kernel gaussiano
-#     sigma = 1.0  # Puedes ajustar este valor
-#     similarities = rbf_kernel(X_reduced, gamma=1.0 / (2.0 * sigma**2))
-
-#     # Visualiza los datos en el nuevo espacio de dimensión reducida
-#     plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c='b', marker='o', alpha=0.5)
-#     plt.title(f'Dimensión reducida d={d}')
-#     plt.show()
-
-# # Similitud utilizando PCA y proyecciones al azar
-# for d in dims:
-#     # Utiliza PCA para reducir la dimensionalidad
-#     pca = PCA(n_components=d)
-#     X_pca = pca.fit_transform(X)
-    
-#     # Genera proyecciones al azar
-#     num_random_projections = 100  # Puedes ajustar este valor
-#     random_projections = np.random.randn(X.shape[1], d)
-#     X_random = X.dot(random_projections)
-    
-#     # Calcula las similitudes utilizando el kernel gaussiano
-#     sigma = 1.0  # Puedes ajustar este valor
-#     similarities_pca = rbf_kernel(X_pca, gamma=1.0 / (2.0 * sigma**2))
-#     similarities_random = rbf_kernel(X_random, gamma=1.0 / (2.0 * sigma**2))
-    
-#     # Plotea las similitudes
-#     plt.figure(figsize=(12, 6))
-#     plt.subplot(1, 2, 1)
-#     plt.imshow(similarities_pca, cmap='hot', interpolation='none')
-#     plt.title(f'Similitud PCA (d={d})')
-#     plt.colorbar()
-    
-#     plt.subplot(1, 2, 2)
-#     plt.imshow(similarities_random, cmap='hot', interpolation='none')
-#     plt.title(f'Similitud Proyecciones al Azar (d={d})')
-#     plt.colorbar()
-    
-#     plt.show()
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -65,6 +8,9 @@ X = data.values  # Convierte los datos a una matriz NumPy
 
 # Valores de d a considerar
 dims = [2, 4, 6, 20, X.shape[1]]  # Incluye la dimensionalidad original (p)
+
+# Definir el valor de sigma para calcular la similitud
+sigma = 1.0  # Puedes ajustar este valor
 
 # Realiza la reducción de dimensionalidad y análisis
 for d in dims:
@@ -80,78 +26,85 @@ for d in dims:
     plt.title(f'Dimensión reducida d={d}')
     plt.show()
 
-#Esto para mi esta mal
-
-# # Similitud utilizando PCA y proyecciones al azar
-# for d in dims:
-#     # Reducción de dimensionalidad con PCA sin scikit-learn
-#     cov_matrix = np.cov(X, rowvar=False)
-#     eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-#     sorted_indices = np.argsort(eigenvalues)[::-1]
-#     eigenvalues = eigenvalues[sorted_indices]
-#     selected_eigenvectors = eigenvectors[:, sorted_indices][:, :d]
-#     P_pca = selected_eigenvectors
-#     X_pca = X.dot(P_pca)
-    
-#     # Genera proyecciones al azar
-#     num_random_projections = 100  # Puedes ajustar este valor
-#     random_projections = np.random.randn(X.shape[1], d)
-#     P_random = random_projections
-#     X_random = X.dot(P_random)
-    
-#     # Calcula las similitudes utilizando el kernel gaussiano
-#     sigma = 1.0  # Puedes ajustar este valor
-#     similarities_pca = np.exp(-np.sum((X_pca[:, None, :] - X_pca) ** 2, axis=2) / (2.0 * sigma**2))
-#     similarities_random = np.exp(-np.sum((X_random[:, None, :] - X_random) ** 2, axis=2) / (2.0 * sigma**2))
-    
-#     # Plotea las similitudes
-#     plt.figure(figsize=(12, 6))
-    
-#     plt.subplot(1, 2, 1)
-#     plt.imshow(similarities_pca, cmap='hot', interpolation='none')
-#     plt.title(f'Similitud PCA (d={d})')
-#     plt.colorbar()
-    
-#     plt.subplot(1, 2, 2)
-#     plt.imshow(similarities_random, cmap='hot', interpolation='none')
-#     plt.title(f'Similitud Proyecciones al Azar (d={d})')
-#     plt.colorbar()
-    
-#     plt.show()
-
-#Esto va mejor pero todavía hay que arreglarlo un toque
-
-# Similitud utilizando PCA y proyecciones al azar
+# Similitud entre muestras en el espacio original y reducido
 for d in dims:
-    # Reducción de dimensionalidad con PCA sin scikit-learn
-    cov_matrix = np.cov(X, rowvar=False)
-    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
-    selected_eigenvectors = eigenvectors[:, sorted_indices][:, :d]
-    P_pca = selected_eigenvectors
-    X_pca = X.dot(P_pca)
+    # Calcula la similitud en el espacio original
+    similarity_original = np.exp(-np.sum((X[:, None, :] - X) ** 2, axis=2) / (2.0 * sigma**2))
     
-    # Genera proyecciones al azar
-    num_random_projections = 100  # Puedes ajustar este valor
-    random_projections = np.random.randn(X.shape[1], d)
-    P_random = random_projections
-    X_random = X.dot(P_random)
-    
-    # Calcula las similitudes utilizando el kernel gaussiano
-    sigma = 1.0  # Puedes ajustar este valor
-    similarities_pca = np.exp(-np.sum((X_pca[:, None, :] - X_pca) ** 2, axis=2) / (2.0 * sigma**2))
-    similarities_random = np.exp(-np.sum((X_random[:, None, :] - X_random) ** 2, axis=2) / (2.0 * sigma**2))
+    # Calcula la similitud en el espacio reducido (usando SVD)
+    U, S, VT = np.linalg.svd(X, full_matrices=False)
+    X_reduced = np.dot(U[:, :d], np.dot(np.diag(S[:d]), VT[:d, :]))
+    similarity_reduced = np.exp(-np.sum((X_reduced[:, None, :] - X_reduced) ** 2, axis=2) / (2.0 * sigma**2))
     
     # Plotea las similitudes como gráficos de dispersión
     plt.figure(figsize=(12, 6))
     
     plt.subplot(1, 2, 1)
-    plt.scatter(X_pca[:, 0], X_pca[:, 1], c='b', marker='o', alpha=0.5)
-    plt.title(f'Similitud PCA (d={d})')
+    plt.scatter(X[:, 0], X[:, 1], c='b', marker='o', alpha=0.5)
+    plt.title('Similitud Espacio Original')
     
     plt.subplot(1, 2, 2)
-    plt.scatter(X_random[:, 0], X_random[:, 1], c='b', marker='o', alpha=0.5)
-    plt.title(f'Similitud Proyecciones al Azar (d={d})')
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c='b', marker='o', alpha=0.5)
+    plt.title(f'Similitud Espacio Reducido (d={d})')
     
     plt.show()
+
+
+#2.2---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Muestra las dimensiones más representativas con respecto a las dimensiones reducidas obtenidas por SVD
+# Muestra las dimensiones más representativas con respecto a las dimensiones reducidas obtenidas por SVD
+for d in dims:
+    # Selecciona las dimensiones VT correspondientes a la reducción d
+    VT_reduced = VT[:d, :]
+
+    # Calcula el promedio de los valores absolutos de cada dimensión de VT_reduced
+    avg_abs = np.mean(np.abs(VT_reduced), axis=1)
+
+    # Ordena las dimensiones en orden descendente según su importancia
+    sorted_dims = np.argsort(avg_abs)[::-1]
+
+    print(f"Dimensiones más importantes para d={d} según SVD:")
+    for i in range(min(5, len(sorted_dims))):  # Muestra las 5 dimensiones más importantes o las disponibles
+        print(f"Dimensión {sorted_dims[i]} - Importancia: {avg_abs[sorted_dims[i]]}")
+
+    print()
+
+
+#1.3-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Carga de datos etiquetas
+Y = pd.read_csv('y2.txt').values
+Y = Y.T  # Transpone Y para ajustar las dimensiones
+
+# Realiza la reducción de dimensionalidad y análisis
+min_error = float('inf')
+best_d = 0
+
+for d in dims:
+    # Calcula la SVD de X
+    U, S, VT = np.linalg.svd(X, full_matrices=False)
+    
+    # Reduce la dimensionalidad seleccionando las d componentes principales
+    X_reduced = np.dot(U[:, :d], np.dot(np.diag(S[:d]), VT[:d, :]))
+
+    beta = np.linalg.lstsq(X_reduced, Y, rcond=None)[0]
+    
+    # Calcula las predicciones
+    predictions = np.dot(X_reduced, beta)
+    
+    # Calcula el error de predicción
+    error = np.linalg.norm(predictions - Y)**2
+    
+    # Guarda el mejor d basado en el error
+    if error < min_error:
+        min_error = error
+        best_d = d
+
+    # Plotea las proyecciones en el nuevo espacio reducido
+    plt.figure(figsize=(6, 6))
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c='b', marker='o', alpha=0.5)
+    plt.title(f'Dimensión reducida d={d}')
+    plt.show()
+
+# Muestra el mejor d y el error mínimo
+print(f"La mejor d que minimiza el error es: {best_d}")
+print(f"El mínimo error es: {min_error}")
